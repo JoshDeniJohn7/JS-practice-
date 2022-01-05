b254a4585b1f4020799425c53f7ed1543a5a35df
JoshDeniJohn7 <94109581+JoshDeniJohn7@users.noreply.github.com>
2022-01-05 16:37:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function add() {
+var result = 
+    Number(document.getElementById("firstno.").value) +
+    
+    Number(document.getElementById("sencondno.").value);
+    window.alert(result);
+    
+}
+
+function subtract() {
+    var result = 
+    Number(document.getElementById("firstno.").value) -
+    
+    Number(document.getElementById("sencondno.").value);
+    window.alert(result);
+}
+
+function divide() {
+    var result = 
+    Number(document.getElementById("firstno.").value) /
+    
+    Number(document.getElementById("sencondno.").value);
+    window.alert(result);
+}
+
+function multiply() {
+    var result = 
+    Number(document.getElementById("firstno.").value) *
+    
+    Number(document.getElementById("sencondno.").value);
+    window.alert(result);
+}
